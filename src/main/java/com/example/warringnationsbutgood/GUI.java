fc05b13a052918720d0c6e0a27e1aefe45f41570
Leon <junzai12@icloud.com>
2023-08-31 18:55:19 -0700

    trying to add a cow

--- src/main/java/com/example/warringnationsbutgood/GUI.java
+++ src/main/java/com/example/warringnationsbutgood/GUI.java
@@ -2,6 +2,7 @@ package com.example.warringnationsbutgood;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.awt.*;
 import java.io.IOException;
@@ -15,11 +16,15 @@ public class GUI extends Application {
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("lobby.fxml"));
         scene = new Scene(fxmlLoader.load(), size.getWidth(), size.getHeight());
+
+        Image img = new Image(getClass().getResourceAsStream("/cow.png"));
+        primaryStage.getIcons().add(img);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Warring Nations");
         primaryStage.setFullScreen(true);
         primaryStage.show();
     }
+
 
     public static double getWidth() { return size.getWidth(); }
 
