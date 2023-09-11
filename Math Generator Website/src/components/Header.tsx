9c5ed03d35008506ca4bc8c1fd4bb050217f7552
Leon <junzai12@icloud.com>
2023-09-10 19:58:10 -0700

    fonts

--- Math Generator Website/src/components/Header.tsx
+++ Math Generator Website/src/components/Header.tsx
@@ -1,7 +1,19 @@
 const Header = () => {
   return (
     <div className="">
-      <h1>Hello World</h1>
+      <h1 id="main">Warring Nations Math Maker</h1>
+
+      <label htmlFor="stages">Choose a stage: </label>
+
+    <select name="stages" id="stages">
+      <option value="arithmetic">Arithmetic</option>
+      <option value="geometry">Geometry</option>
+      <option value="algebruh">Algebruh</option>
+      <option value="calculus">Calculus</option>
+      <option value="abstract">Abstract</option>
+    </select>
+
+    <button>Go</button>
     </div>
   )
 }
