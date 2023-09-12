6cf064409b01efdab9e9fe30b1c9769f5aca3512
Leon <junzai12@icloud.com>
2023-09-12 12:51:30 -0700

    mathjax works once and doesnt work again

--- Math Generator Website/src/components/Generate.tsx
+++ Math Generator Website/src/components/Generate.tsx
@@ -1,17 +1,46 @@
+const arithmetic = () => {
+
+}
+
+const geometry = () => {
+  
+}
+
+const algebruh = () => {
+  
+}
+
+const calculus = () => {
+  
+}
+
+const abstract = () => {
+  
+}
+
+const stages: string[] = ["arithmetic", "geometry", "algebruh", "calculus", "abstract"];
+const stageFunctions: void[] = [arithmetic(), geometry(), algebruh(), calculus(), abstract()];
+let answers: number[] = [];
+
 const Generate = (props: any) => {
     const stage: string = props.stage;
-    let image: string = "";
-    let answers: number[] = [];
+
+    for (let i: number = 0; i < stages.length; i++) {
+      if (stages[i] === stage) {
+        stageFunctions[i];
+        break;
+      }
+    }
 
     return (  
        <div>
         <p><span className="math display">\[y = \frac&#123;a&#125;&#123;b&#125; + c^2 + d\]</span></p>
          <div className="flex">
-         <img id="problem" width="200" src="https://miro.medium.com/v2/resize:fit:1400/1*L76A5gL6176UbMgn7q4Ybg.jpeg" alt="photo of math" />
          </div>
 
       <div className="flex">
       <form action="">
+        
           <h2>Select an answer: </h2>
           {answers[0]}<input type="radio" name="answer" value="" /> <br />
           {answers[1]}<input type="radio" name="answer" value="" /> <br />
