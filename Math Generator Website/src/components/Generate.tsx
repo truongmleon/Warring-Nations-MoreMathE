cf5d63712cf6b69414af8112bcacdac37f135d42
Leon <junzai12@icloud.com>
2023-09-12 10:56:40 -0700

    image centering

--- Math Generator Website/src/components/Generate.tsx
+++ Math Generator Website/src/components/Generate.tsx
@@ -3,18 +3,23 @@ const Generate = (props: any) => {
     let image: string = "";
     let answers: number[] = [];
     return (  
+       <div>
+         <div className="flex">
+         <img id="problem" width="200" src="https://miro.medium.com/v2/resize:fit:1400/1*L76A5gL6176UbMgn7q4Ybg.jpeg" alt="photo of math" />
+         </div>
+
       <div className="flex">
-        <img width="200" src="https://miro.medium.com/v2/resize:fit:1400/1*L76A5gL6176UbMgn7q4Ybg.jpeg" alt="photo of math" />
       <form action="">
           <h2>Select an answer: </h2>
-          <input type="radio" /> <br />
-          <input type="radio" /> <br />
-          <input type="radio" /> <br />
-          <input type="radio" /> <br />
+          {answers[0]}<input type="radio" /> <br />
+          {answers[1]}<input type="radio" /> <br />
+          {answers[2]}<input type="radio" /> <br />
+          {answers[3]}<input type="radio" /> <br />
       </form>
   
       <button className="go">Enter</button>
       </div>
+       </div>
     )
   }
   
