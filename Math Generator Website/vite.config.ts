64cc0e7f6d50468a6f975122c904a416436b42e2
Leon <junzai12@icloud.com>
2023-09-22 21:03:25 -0700

    Update vite.config.ts

--- Math Generator Website/vite.config.ts
+++ Math Generator Website/vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    rollupOptions: {
+      input: {
+        arithmetic: 'arithmetic.html',
 
+      },
+    },
   },
 })
